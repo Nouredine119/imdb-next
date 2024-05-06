1ea52bd4f49744c701c55631ff9844f3c9a2c4d6
Nouredine119 <nourdinachlafta14@gmail.com>
2024-05-06 15:38:03 +0100

    push project

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,4 +1,3 @@
-
 export default function loading() {
   return (
     <div className='flex justify-center'>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,22 +3,31 @@ import Link from 'next/link';
 import { FiThumbsUp } from 'react-icons/fi';
 
 export default function Card({ result }) {
+
   return (
     <div className='group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200'>
       <Link href={`/movie/${result.id}`}>
-        <Image
-          src={`https://image.tmdb.org/t/p/original/${
-            result.backdrop_path || result.poster_path
-          }`}
-          width={500}
-          height={300}
-          className='sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300'
-        ></Image>
+        {
+          (result.backdrop_path || result.poster_path) ?
+          (<Image
+            src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path
+              }`}
+            alt="PicMovie"
+            width={500}
+            height={300}
+            className='sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300'
+          ></Image> ) :(
+            <img src="/assets/question.jpg" className="w-[500px] h-[300px] sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300" alt='inco'/>
+
+          )
+        }
+
         <div className='p-2'>
           <p className='line-clamp-2 text-md'>{result.overview}</p>
           <h2 className='text-lg font-bold truncate'>
             {result.title || result.name}
           </h2>
+          
           <p className='flex items-center'>
             {result.release_date || result.first_air_date}
             <FiThumbsUp className='h-5 mr-1 ml-3' />
